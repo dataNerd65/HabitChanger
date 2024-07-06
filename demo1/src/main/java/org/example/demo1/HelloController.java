40acdea085daa10211cfe589d0038bdf692a7fcb
dataNerd65 <pkiarie811@gmail.com>
2024-07-06 13:22:32 +0300

    working on controls of login

--- demo1/src/main/java/org/example/demo1/HelloController.java
+++ demo1/src/main/java/org/example/demo1/HelloController.java
@@ -1,14 +1,32 @@
 package org.example.demo1;
 
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Hyperlink createAccount;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onHyperlinkClick(ActionEvent event){
+        System.out.println("Hyperlink Clicked!");
+        try{
+            //Loading the FXML for the account creation view
+            Parent accountCreationView = FXMLLoader.load(getClass().getResource("/org/example/demo1/AccountCreation.fxml"));
+            Scene accountCreationScene = new Scene(accountCreationView);
+
+            //Getting the current stage from the event source and set the new scene
+            Stage window = (Stage) createAccount.getScene().getWindow();
+            window.setScene(accountCreationScene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
