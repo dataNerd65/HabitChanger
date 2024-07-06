14d9d328bae55b418f555bb8412feb1a9f5e8307
dataNerd65 <pkiarie811@gmail.com>
2024-07-06 18:14:28 +0300

    Implemented login and clear on click functions

--- demo1/src/main/java/org/example/demo1/HelloController.java
+++ demo1/src/main/java/org/example/demo1/HelloController.java
@@ -7,9 +7,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class HelloController {
     @FXML
@@ -28,5 +34,62 @@ public class HelloController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+    //Handling the submit button click
+    @FXML
+    private void onSubmitButtonClick(ActionEvent event){
+        System.out.println("Submit button hit!");
+        String user = username.getText();
+        String pass = password.getText();
+
+        if(user.isEmpty() || pass.isEmpty()){
+            APPutils.ShowAlert("Login Error", "Username and Password cannot be empty.");
+            return;
+        }
+
+        try(Connection conn = DatabaseUtil.getConnection()){
+            if(conn != null){
+                PreparedStatement statement = conn.prepareStatement("SELECT * FROM users2 WHERE username = ? AND password = ?");
+                statement.setString(1, user);
+                statement.setString(2, pass);
+
+                ResultSet rs = statement.executeQuery();
+                if (rs.next()) {
+                    //Login Success
+                    APPutils.ShowAlert("Success!", "Login Success.");
+                    //Navigating to the next scene
+                    try{
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/demo1/Workspace.fxml"));
+                        Parent workspaceView = loader.load();
+                        Scene  workspaceScene = new Scene(workspaceView, 800, 600);
+
+                        //Getting current Stage
+                        Stage window = (Stage) createAccount.getScene().getWindow();
+                        window.setScene(workspaceScene);
+                        window.show();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+
+                }else {
+                    //Login Failure
+                    APPutils.ShowAlert("Login Error!", "Invalid username or password");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            APPutils.ShowAlert("Database Error!", "An error occurred while accessing the database.");
+        }
+    }
+    @FXML
+    private void onClearButtonClick(ActionEvent event){
+        System.out.println("Clear Button hit!");
+        username.setText("");
+        password.setText("");
     }
 }
