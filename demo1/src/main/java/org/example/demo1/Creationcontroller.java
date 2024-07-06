102a6ec1f854cbc489ee5d7606316ddebf74cff4
dataNerd65 <pkiarie811@gmail.com>
2024-07-06 13:21:17 +0300

    Working on the account creation controller

--- demo1/src/main/java/org/example/demo1/Creationcontroller.java
+++ demo1/src/main/java/org/example/demo1/Creationcontroller.java
@@ -1,16 +1,47 @@
 package org.example.demo1;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class Creationcontroller extends Application {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        launch(args);
+public class Creationcontroller {
+    @FXML
+    private Hyperlink backToLogin;
+    @FXML
+    private void onHyperlinkClick(ActionEvent event){
+        System.out.println("backToLogin Link Pressed!");
+        try {
+            //Loading the FXML for the going back tologin page
+            Parent LoginView = FXMLLoader.load(getClass().getResource("/org/example/demo1/hello-view.fxml"));
+            Scene LoginScene = new Scene(LoginView);
+
+            //Getting the current stage from the event source and set the new scene
+            Stage window = (Stage) backToLogin.getScene().getWindow();
+            window.setScene(LoginScene);
+        } catch(IOException e){
+            e.printStackTrace();
+
+        }
     }
+    @FXML
+    private TextField Fname;
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField email;
+    @FXML
+    private PasswordField password1;
+    @FXML
+    private PasswordField password2;
 
-    @Override
-    public void start(Stage primaryStage) {
 
-    }
 }
