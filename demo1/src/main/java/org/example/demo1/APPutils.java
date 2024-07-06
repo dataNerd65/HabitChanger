43c02d6736565e384154834dcb5e4c80b47064ce
dataNerd65 <pkiarie811@gmail.com>
2024-07-06 17:05:20 +0300

    Implemented first the method of a timed alert

--- demo1/src/main/java/org/example/demo1/APPutils.java
+++ demo1/src/main/java/org/example/demo1/APPutils.java
@@ -1,5 +1,23 @@
 package org.example.demo1;
 
+import javafx.animation.PauseTransition;
+import javafx.scene.control.Alert;
+import javafx.util.Duration;
+
+import static javafx.util.Duration.seconds;
+
 public class APPutils {
+    public static void ShowAlert(String headerText, String contextText){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contextText);
+        alert.show();
+
+        //closing the alert after 3 seconds
+        PauseTransition delay = new PauseTransition(Duration.seconds(3));
+        delay.setOnFinished(event -> alert.close());
+        delay.play();
+
+    }
 
 }
