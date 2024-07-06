4ab5a55b676363d365c8a1fae814edfce55c30ef
dataNerd65 <pkiarie811@gmail.com>
2024-07-06 14:19:24 +0300

    Making sure my project recognises it needs sql

--- demo1/src/main/java/module-info.java
+++ demo1/src/main/java/module-info.java
@@ -10,6 +10,8 @@ module org.example.demo1 {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
+    requires java.sql;
 
     opens org.example.demo1 to javafx.fxml;
     exports org.example.demo1;
