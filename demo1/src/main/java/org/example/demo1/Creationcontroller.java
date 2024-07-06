84142e618ad07b5b703c4dfe6f7643d5bc813635
dataNerd65 <pkiarie811@gmail.com>
2024-07-06 10:46:24 +0300

    The controls of the signup page

---
+++ demo1/src/main/java/org/example/demo1/Creationcontroller.java
@@ -0,0 +1,16 @@
+package org.example.demo1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Creationcontroller extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
