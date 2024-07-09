6f8cd641be84f573c5d1465c28a39c8f4bbfbe04
dataNerd65 <pkiarie811@gmail.com>
2024-07-09 10:56:45 +0300

    Some mods, am a bit lost, but i will find myself

--- demo1/src/main/java/org/example/demo1/Workspace.java
+++ demo1/src/main/java/org/example/demo1/Workspace.java
@@ -1,10 +1,7 @@
 package org.example.demo1;
 
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
@@ -32,45 +29,16 @@ public class Workspace extends BorderPane {
         //  Dynamic width calculation for sidebar based on BorderPane's width
         this.widthProperty().addListener((observable, oldValue, newValue) ->{
             double paneWidth = newValue.doubleValue();
-            sidebar.setPrefWidth(paneWidth * 0.05); //setting sidebar to 10% of total width
+            sidebar.setPrefWidth(paneWidth * 0.1); //setting sidebar to 10% of total width
         });
+        //Create buttons
         
         HBox upBar = new HBox(10);
         upBar.setPrefWidth(50);
         //Adding some css
-        sidebar.setStyle("-fx-background-color: #523722");
+        sidebar.setStyle("-fx-background-color: rgba(122,157,185,255)");
         upBar.setStyle("-fx-background-color: #e6f2f0; -fx-border-color: black; -fx-border-width: 2px;");
-        //Load the icons
-        Image homeImage = new Image(getClass().getResourceAsStream("/org/example/demo1/images/home.png"));
-        Image settingsImage = new Image(getClass().getResourceAsStream("/org/example/demo1/images/settings.png"));
-        Image logoutImage = new Image(getClass().getResourceAsStream("/org/example/demo1/images/logout.png"));
-        Image profileImage = new Image(getClass().getResourceAsStream("/org/example/demo1/images/user.png"));
-
-
-        //creating imageview for each icon
-        ImageView homeIcon = new ImageView(homeImage);
-        ImageView settingsIcon = new ImageView(settingsImage);
-        ImageView logoutIcon = new ImageView(logoutImage);
-        ImageView profileIcon = new ImageView(profileImage);
-
-        //Adjusting the icon size
-        homeIcon.setFitHeight(40);
-        homeIcon.setFitWidth(40);
-        settingsIcon.setFitHeight(40);
-        settingsIcon.setFitWidth(40);
-        logoutIcon.setFitHeight(40);
-        logoutIcon.setFitWidth(40);
-        profileIcon.setFitHeight(40);
-        profileIcon.setFitWidth(40);
-
-        //Creating buttons with icons
-        Button homeButton = new Button("", homeIcon);
-        Button settingsButton = new Button("", settingsIcon);
-        Button logoutButton = new Button("", logoutIcon);
-        Button profileButton = new Button("", profileIcon);
-
-        // Add buttons or other controls to the sidebar
-        sidebar.getChildren().addAll(homeButton,profileButton, settingsButton, logoutButton);
+//
 
         // Set the sidebar to the left of the main layout
         this.setLeft(sidebar);
@@ -87,7 +55,5 @@ public class Workspace extends BorderPane {
         upBar.getChildren().addAll(spacerleft, headerLabel, spacerRight);
         this.setTop(upBar);
 
-        // Other main content can be added to the center
-        // this.setCenter(...);
     }
 }
